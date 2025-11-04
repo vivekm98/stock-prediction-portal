f641fd0c5ebebe2cd57ca2071434e4c08bb178fa
Vivek Ramesh More <morevivek857@gmail.com>
2025-11-03 22:11:07 -0800

    homepage react

---
+++ frontend-react/stock-prediction-portal/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './assets/css/style.css'
+import Header from './assets/components/Header'
+import Main from './assets/components/Main'
+import Footer from './assets/components/Footer'
+
+
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+ 
+
+
+    </>
+  )
+}
+
+export default App
---
+++ frontend-react/stock-prediction-portal/src/assets/components/Main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Button from './Button'
+
+const Main = () => {
+  return (
+    <>
+     <div className="container">
+      <div className='p-5 text-center bg-light-dark rounded'>
+        <h1 className='text-light'> Stock Prediction Portal</h1>
+        <p className='text-light lead'>Stock Market Prediction Portal provides users with data-driven insights to help make smarter investment decisions. By analyzing historical market data, trends, and financial indicators, the system predicts potential stock movements with improved accuracy. The platform combines advanced machine learning algorithms and real-time market updates to deliver actionable forecasts and intuitive visualizations. Whether you're a beginner exploring the market or an experienced trader, this portal offers valuable tools to support informed and strategic trading decisions.</p>
+        <Button text ="Login" class ="btn-outline-info" />
+      </div>
+     </div>
+    </>
+  )
+}
+
+export default Main
